15b9b47177f88eff535991f9d21295bfe5ebcb31
btthl <davin.nguyen19@gmail.com>
2023-04-23 16:31:49 -0700

    removed quit option from pause menu, fixed progress menu blocking buttons

--- ui/menus/pause_menu.gd
+++ ui/menus/pause_menu.gd
@@ -1,12 +1,10 @@
 extends Menu
 
 onready var resume_button : Button = $Content/Resume
-onready var quit_button : Button = $Content/Quit
 
 
 func _ready() -> void:
 	resume_button.connect("pressed", self, "close")
-	quit_button.connect("pressed", self, "quit")
 
 
 func open() -> void:
@@ -18,8 +16,3 @@ func open() -> void:
 func close() -> void:
 	.close()
 	get_tree().paused = false
-
-
-func quit() -> void:
-	get_tree().quit()
-	pass
