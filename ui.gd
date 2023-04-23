dc0c4f7bc483c96b50906fd3316decaf9ea366e6
btthl <davin.nguyen19@gmail.com>
2023-04-22 23:52:41 -0700

    renamed windows to menus for consistency

--- ui.gd
+++ ui.gd
@@ -1,16 +1,23 @@
 extends Control
 
 onready var cam : Camera2D = $"../../Cam"
+onready var menus : Array = get_children()
 
 var in_transition : bool = false
+
+
+func _ready() -> void:
+	for menu in menus:
+		menu.visible = false
+
 
 func open_ui(ui_name : String) -> void:
 	# TODO implement
 	var selected_ui : Control = get_node(ui_name)
 
-	for window in get_children():
-		if window.name != ui_name:
-			window.visible = false
+	for menu in menus:
+		if menu.name != ui_name:
+			menu.visible = false
 		else:
-			window.visible = !window.visible
+			menu.visible = !menu.visible
 
